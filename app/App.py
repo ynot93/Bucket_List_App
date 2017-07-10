f00293124fe2601affd8e7f8a8f52ac309e12eac
Tony Mputhia <tonymmputhia@gmail.com>
2017-07-10 21:28:57 +0300

    changed app template names

--- app/App.py
+++ app/App.py
@@ -5,19 +5,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template(index.html)
+    return render_template('index.html')
 
 @app.route('/Sign_Up', methods=['GET', 'POST'])
 def sign_up():
-    return render_template(signup.html)
+    return render_template('signup.html')
 
 @app.route('/Profile')
 def profile():
-    return render_template(profile.html)
+    return render_template('profile.html')
 
 @app.route('/Create_List')
 def create_list():
-    return render_template(createlist.html)
+    return render_template('createlist.html')
 
 if __name__ == '__main__':
     app.run()
