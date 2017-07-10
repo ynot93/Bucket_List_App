c85e0d920d531c4a53880bb380a25262c821f371
Tony Mputhia <tonymmputhia@gmail.com>
2017-07-10 20:53:26 +0300

    Created static and template folders

--- App.py
+++ App.py
@@ -1,1 +1,25 @@
-__author__ = 'JANE'
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/', methods=['GET', 'POST'])
+def Sign_Up():
+    return
+
+
+if __name__ == '__main__':
+    app.run()
+
+
+class User:
+    def __init__(self, first_name, last_name, password):
+        self.__firstName = first_name
+        self.__lastName = last_name
+        self.__Password = password
+
+
